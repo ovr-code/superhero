c6b0bc23d7ecfd9088db5a6e9e131980af23d7c3
Pruthviraj Chauhan <yrgdstudios@gmail.com>
2023-01-19 09:40:19 +0530

    v2.0 Version of the app
    Logs  - 
    •Search and History features in the app✅
    •No changes in UI❌

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
-const superHero_token = '179816364683636' //Personal taken to get images
-const sh_url = `https://superheroapi.com/api.php/${superHero_token}` //url of Superhero image with application of token which is dynamic!
+const superHero_token = "179816364683636"; //Personal taken to get images
+const sh_url = `https://superheroapi.com/api.php/${superHero_token}`; //url of Superhero image with application of token which is dynamic!
 
-console.log(sh_url) //Just to confirm the url is right and everything is fine!
-const heroimage = document.getElementById('superheroDiv') //Getting the html id
-const heroName = document.getElementById('heroName') //Getting the html id
+// console.log(sh_url); //Just to confirm the url is right and everything is fine!
 
-const button = document.getElementById('buttonClick') //Getting the html id of button
+const heroimage = document.getElementById("superheroDiv"); //Getting the html id
+const heroName = document.getElementById("heroName"); //Getting the html id
+const heroHistory = document.getElementById("heroHistory"); //Getting the html id
+const randomButton = document.getElementById("randomButton"); //Getting the html id of button
+const searchButton = document.getElementById("searchbutton"); //Getting the html id of button
+const searchInput = document.getElementById("searchInput");
 
 // For generating a random number between some likely numbers eg: 1 to 10000
-const randomNumber = (max, min) =>
-  Math.floor(Math.random() * (max - min + 1) + min)
+// [const randomNumber = (max, min) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);]  //This is the function to generate random number but depraicated!
 // console.log(randomNumber(1,9999))
 
 // The function where real magic happens and tried everything to be Dynamic!
 
-const superHero = (id) => {
+// Searching Super Hero
+const getSearchedSuperHero = (heroName) => {
+  fetch(`${sh_url}/search/${heroName}`)
+    .then((response) => response.json())
+    .then((json) => {
+      hero = json.results[0];
+
+      heroimage.innerHTML = `
+      <img class='superhero_img' src='${hero.image.url}'/>
+     <div class='info-side'> <h2>${hero.name} </h2> 
+     <p><strong>Publisher: </strong> ${hero.biography.publisher}</p>
+     <p><strong>Work: </strong>${hero.work.base}</p>
+     
+     <p><strong>Gender: </strong>${hero.appearance.gender} <br>
+      <strong>Occupation: </strong>${hero.work.occupation}</p> 
+      
+      <h3>:Other Details:</h3>
+      <p>
+     <bold> Connections: </bold>  
+     <h3>Coming Soon..... </h3>
+     
+       
+      </p> 
+      </div>
+
+      `;
+
+      // History Section
+      heroHistory.innerHTML += `
+       <div>
+       <h3 class='history_name'>${hero.name}</h3>
+       <img class='superhero_img_small' src='${hero.image.url}'/></div>
+       `;
+
+      document.title = `${hero.name} | ovrcode`;
+    });
+};
+
+// getSearchedSuperHero();
+
+// Random Hero Function
+const getRandomSuperHero = (id) => {
   fetch(`${sh_url}/${id}`) //Fetch or recive the URL (Something called promises and asynchronous javascript😍!)
     .then((response) => response.json()) //responses the JSON file  from the url!
     .then((json) => {
-      //Here I am gonna playing with JSOn and make the webpage something Funn!
-      console.log(json.image.url)
-      console.log(json.name)
-      console.log(json.id)
-      console.log(json)
-      console.log(json.appearance)
+      // Change the button Name as once it starts picking random data!
+      randomButton.innerHTML = "Another Super Hero";
 
-      // heroName.innerHTML = `--`
-      button.innerHTML = 'Another Super Hero'
-
+      // Functions to get the Random data from the JSON file and display it on the webpage!
       heroimage.innerHTML = `
       <img class='superhero_img' src='${json.image.url}'/>
      <div class='info-side'> <h2>${json.name} </h2> 
@@ -46,16 +84,45 @@ const superHero = (id) => {
       </p> 
       </div>
 
-      `
-      document.title = `${json.name} | ovrcode`
-    })
-}
+      `;
 
-button.onclick = () => {
-  let number = randomNumber(1, 732)
-  superHero(number)
-}
+      // History Section
+      heroHistory.innerHTML += `
+       <div>
+       <h3 class='history_name'>${json.name}</h3>
+       <img class='superhero_img_small' src='${json.image.url}'/></div>
+       `;
+
+      document.title = `${json.name} | ovrcode`;
+    });
+};
+
+// Random number generator function
+const randomHero = () => {
+  const totalHero = 731;
+  const number = Math.floor(Math.random() * totalHero) + 1;
+  return number;
+};
+
+//Onlick Random Button
+randomButton.onclick = () => {
+  // let number = randomNumber(1, 732)
+  console.log(randomHero());
+  getRandomSuperHero(randomHero());
+};
+
+//Onclick Search Button
+
+searchButton.onclick = () => {
+  const searched = searchInput.value;
+  console.log(searched);
+  getSearchedSuperHero(searched);
+};
 
 // https://superheroapi.com/api/179816364683636
 
 // document.querySelector('body').innerHTML += ` <img src="${}"`
+
+// v.1.1 Updates
+
+// 1. Added the random number generator function to generate random number between 1 to 731
